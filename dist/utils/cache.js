dbd21226a29b64dcb98ced39f24c7c1ddc541e8b
Juan David Marcillo Alba <juan.marcillo@globant.com>
2021-12-21 11:43:25 -0500

    Removing gitignore files/folders

--- dist/utils/cache.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cacheResponse = (res, seconds) => {
-    res.set("Cache-Control", `public, max-age=${seconds}`);
-};
-exports.default = cacheResponse;
